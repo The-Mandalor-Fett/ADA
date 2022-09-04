3eb440442f408e285263bd526597bfa3656858ac
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-09-03 23:51:39 -0500

    Creamos la función para generar numeros aleatorios

--- InsertionSort.c
+++ InsertionSort.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+void numerosAleatorios(int arregloRecibido[],int numElementosRecibido);
+
+void numerosAleatorios(int arregloRecibido[],int numElementosRecibido)
+{
+    
+}
 int main(int argc, char const *argv[])
 {
-    int numeroElemntos = 0; 
-    printf("Por favor introduce un numero: ");
-    scanf("%d",&numeroElemntos);
-    int array[numeroElemntos];  
-    // GENERA NÚMEROS ALEATORIOS PARA EL ARRAY
-    for (int i = 0; i < numeroElemntos; i++)
-    {
-        array[i] = rand()%100;  // VA DE 0 A 99
-        // printf("Array %d Valor %d\n",i,array[i]);
-    }
-    
+    srand(time(NULL));
+    int numeroDeElementos = 0;
+    printf("Por favor introduce el numero de elementos del arreglo: ");
+    scanf("%d",&numeroDeElementos);
+    int arreglo[numeroDeElementos];
 
     system("pause");
     return 0;
