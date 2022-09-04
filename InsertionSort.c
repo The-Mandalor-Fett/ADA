da6b0d82855328bb4e25ee7c5dc38e27785e820d
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-09-03 21:10:39 -0500

    Generamos los números aleatorios

--- InsertionSort.c
+++ InsertionSort.c
@@ -3,6 +3,17 @@
 
 int main(int argc, char const *argv[])
 {
+    int numeroElemntos = 0; 
+    printf("Por favor introduce un numero: ");
+    scanf("%d",&numeroElemntos);
+    int array[numeroElemntos];  
+    // GENERA NÚMEROS ALEATORIOS PARA EL ARRAY
+    for (int i = 0; i < numeroElemntos; i++)
+    {
+        array[i] = rand()%100;  // VA DE 0 A 99
+        // printf("Array %d Valor %d\n",i,array[i]);
+    }
+    
 
     system("pause");
     return 0;
