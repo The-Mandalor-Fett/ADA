0a9d2445efcd1e97f9ea4076de10e6be337f09da
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-10-25 23:11:06 -0500

    Creamos la función de npumeros aleatorios

--- selectionSrot2.c
+++ selectionSrot2.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+// DECLARACIÓN DE FUNCIONES
+void numerosAleatorios(int arregloRecibido[],int numElementosRecibido); //GENERA NUMEROS ALEATORIOS PARA EL ARRAY
+void ordenamientoSelecion(int arregloRecibido[], int tamanioArray); // FUNCION QUE ORDENA EL ARRAY
+
+//DEFINICIÓN DE FUNCIONES
+void numerosAleatorios(int arregloRecibido[],int numElementosRecibido)
+{
+    for (int i = 0; i < numElementosRecibido; i++)
+    {
+        arregloRecibido[i] = rand()%100; //LLENA UN ARREGLO DE 0 A 99
+        // AL SER UN arreglo ALGO PARECIDO A UN APUNTADOR 
+        // PUEDE MODIFICAR LOS VALORES DE NUESTRO VALOR EN MAIN
+    }
+    
+}
+
+
 
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-    
+
     return 0;
 }
