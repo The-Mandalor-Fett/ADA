1878d0a95806c9b7c1326d9ebb6878ef6808bfb6
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-11-08 01:05:28 -0600

    Definición de funciones ordenamiento y números aleatorios

--- quickSort.c
+++ quickSort.c
@@ -10,11 +10,30 @@ int quickSort(int arreglorecibido[], int inicioArray, int finArray); // DIVIDE E
 
 int main(int argc, char const *argv[])
 {
+    srand(time(NULL));
     int numeroElementos = 0;
     int *arreglo = NULL;
     printf("Por favor introduce un número de elementos : ");
     scanf("%d", &numeroElementos);
     arreglo = (int *)malloc(numeroElementos * sizeof(int));
-    
+    numerosAleatorios(arreglo, numeroElementos);
+    printf("Arreglo sin ordenar ");
+    for (int i = 0; i < numeroElementos; i++)
+    {
+        printf("%d, ",arreglo[i]);
+    }
+    printf("\n \n");
+    ordenamientoRapido(arreglo, numeroElementos);
+
     return 0;
+}
+
+// DEFINICIÓN DE FUNCIONES
+void numerosAleatorios(int arregloRecibido[], int numElementosRecibido)
+{
+    for (int i = 0; i < numElementosRecibido; i++)
+    {
+        arregloRecibido[i] = rand()%100; //LLENAMOS EL ARREGLO CON NÚMEROS DEL 0 AL 99
+    }
+    
 }
